b3a3a1a4e7a7653240b14cadc34ddd410a98d489
Munim <munimkhanmyself+git@gmail.com>
2023-11-02 09:11:01 +0600

    Add new file

---
+++ JavaScript-1.0/1.3_ConversionOperation.js
@@ -0,0 +1,25 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " munim"  // space, jeno jog korle maje space thake
+
+let str3 = str1 + str2
+console.log(str3);
+
+// baje code
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2") 
+
+// console.log((3 + 4) * 5 % 3)  // best 
