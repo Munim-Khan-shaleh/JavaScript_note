fce81ad4053ffcf663ba018bb7231c34abab9876
Munim <munimkhanmyself+git@gmail.com>
2023-11-05 11:29:02 +0600

    Add variables and datatypes

---
+++ JavaScript-2.0/2.1_Variables.js
@@ -0,0 +1,36 @@
+
+let accountName = "Munim_Khan"
+const accountId = 143552
+let accountEmail = "munim@gmail.com"
+let accountPassword = "786895"
+let accountCity = "Barguna"
+
+console.log(accountEmail)
+console.table([accountName, accountId, accountPassword, accountEmail, accountCity])
+
+/* 
+Prefer not to use var
+because of issue in block scope and functional sc
+*/
+
+// **********DATATYPES************
+
+"use strict";  // treat all js code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+let name = "munim"          // string
+let age = 18                // number
+let isLoggedIn = false      // boolean
+let state;                  // undefined (ja define kora hy nai, value nai.)
+
+
+// number => 2 to power 53
+// biging => eta boro number jemon trading e use hy.
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof age) // jekono value er type janor znz
