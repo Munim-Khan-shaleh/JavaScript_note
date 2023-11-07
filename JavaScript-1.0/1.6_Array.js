ffe9d1a8db0252d2ce1ee4c72e80ce15ccf4972b
Munim <munimkhanmyself+git@gmail.com>
2023-11-07 12:22:02 +0600

    Add some arrays

--- JavaScript-1.0/1.6_Array.js
+++ JavaScript-1.0/1.6_Array.js
@@ -36,3 +36,27 @@ console.log("B", myArr)
 const myn2 = myArr.splice(1, 3)  // 
 console.log("c", myArr)
 console.log(myn2)
+
+// more Array
+
+const marvel_heros = ["thor", "Ironmar", "batman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+// console.log(marvel_heros[3][1])
+
+const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros)
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+// console.log(all_new_heros)
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3))
